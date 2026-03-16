e4f6874c75b597e91eefcca3b1dcf020b1da6c81
Ashmit-101 <ashmitjha3@gmail.com>
2026-03-16 13:48:02 -0400

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Ashmit Jha on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
